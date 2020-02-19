dce814d9d6005fcb6aef9a0c88714b8de28107fa
Adarsh Pawar <adarsh.pawar@bcone.com>
2020-02-19 17:12:40 +0530

    using coursees.component.html

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,29 +1,28 @@
 import { CoursesService } from './courses.service';
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: `
-        <h1>{{ getTitle() }}</h1>
-        <ul>
-          <li *ngFor="let course of courses"> 
-            {{ course }}
-          </li>
-        </ul>
-        <button class="btn btn-primary" [class.active]="isActive">Save</button>
-        `,
-        // *ngFor is a "structural directive". Structural directives shape or reshape the DOM's structure, typically by adding, removing, and manipulating the elements to which they are attached. Directives with an asterisk, *, are structural directives.
-  // templateUrl: './courses.component.html',
+  // template: `
+  //       <h1>{{ getTitle() }}</h1>
+  //       <ul>
+  //         <li *ngFor="let course of courses"> 
+  //           {{ course }}
+  //         </li>
+  //       </ul>
+  //       <button class="btn btn-primary" [class.active]="isActive">Save</button>
+  //       `,
+  // // *ngFor is a "structural directive". Structural directives shape or reshape the DOM's structure, typically by adding, removing, and manipulating the elements to which they are attached. Directives with an asterisk, *, are structural directives.
+  templateUrl: './courses.component.html',
   // styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent{ 
-  
-    title = "List of courses";
-    getTitle = () => this.title;
-    courses;
-    isActive = true;
-    constructor(service: CoursesService) {
-      // let service = new CoursesService();
-      this.courses = service.getCourses();
-    }
+export class CoursesComponent {
+  title = "List of courses";
+  getTitle = () => this.title;
+  courses;
+  isActive = true;
+  constructor(service: CoursesService) {
+    // let service = new CoursesService();
+    this.courses = service.getCourses();
+  }
 }
